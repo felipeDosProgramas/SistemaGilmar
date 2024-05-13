4f64acb61fa2946d2ca3b9f600744725829e48f0
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-13 00:16:20 -0300

    preparing the EntradaUsuario.cs to be tested.

--- src/EntradaUsuario.cs
+++ src/EntradaUsuario.cs
@@ -1,12 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using SistemaDoGilmar.Enums;
 
 namespace SistemaDoGilmar;
 
 public class EntradaUsuario
 {
+    public string Entrada { get; set; }
+
     public EntradaUsuario(string entrada, TipoEntrada tipoEntrada)
     {
+        this.Entrada = entrada.ToLower();
+        if (tipoEntrada == TipoEntrada.NotacaoExpoenteLinguagensProgramacao)
+        {
 
+        }
     }
+
+    private Dictionary<int, string> DividirComNotacaoLp()
+    {
+        string[] entrada = Entrada.Split('x');
+        Console.WriteLine(entrada.ToString());
+        throw new NotImplementedException();
+    }
+
 }
