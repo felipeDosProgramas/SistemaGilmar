88b0b41429e885ec77ad892b11d81e9514da2a3a
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-17 02:35:10 -0300

    Criados os tipos de Exceções que serão tratados posteriormente

---
+++ src/Exceptions/NonQuadraticEquationException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SistemaDoGilmar.Exceptions;
+
+public class NonQuadraticEquationException : Exception
+{
+    public NonQuadraticEquationException() { }
+    public NonQuadraticEquationException(string message) : base(message) { }
+    public NonQuadraticEquationException(string message, Exception inner) : base(message, inner) { }
+}
---
+++ src/Exceptions/NonSimplifiedEquationException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SistemaDoGilmar.Exceptions;
+
+public class NonSimplifiedEquationException : Exception
+{
+    public NonSimplifiedEquationException() { }
+    public NonSimplifiedEquationException(string message) : base(message) { }
+    public NonSimplifiedEquationException(string message, Exception inner) : base(message, inner) { }
+}
