5d7a31b11ea266e4bb932f03f6b18b4165ebf8d4
felipeluizmsouza@gmail.com <felipeluizmsouza@gmail.com>
2024-05-18 03:13:24 -0300

    Criada a calculadora de força em uma classe só

---
+++ src/CalculadoraDeFormulas/Forca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace SistemaDoGilmar.CalculadoraDeFormulas;
+
+public class Forca
+{
+    public Forca()
+    {
+        tentar:
+        try
+        {
+            Console.WriteLine("digite a massa em gramas e a aceleração em m/s \n divida-os com um espaço");
+            CalcularEMostrarForca();
+        }
+        catch (Exception)
+        {
+            Console.Clear();
+            Console.WriteLine("digite como foi pedido, não invente moda");
+            goto tentar;
+        }
+    }
+
+    private void CalcularEMostrarForca() => Console.WriteLine(
+        Console.ReadLine()!
+            .Split(' ')
+            .Select(Convert.ToDouble)
+            .Aggregate((m, a) => m * a)
+    );
+}
